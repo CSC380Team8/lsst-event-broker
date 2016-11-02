f0ce63a7e541ceef81923b73133aa1184e3e23ca
christian sumano <christian sumano>
2016-11-02 13:21:20 -0400

    transient observation data

---
+++ lsstbroker/transientObservation.py
@@ -0,0 +1,19 @@
+"""
+
+This class will get the time value and color observation from each transient
+and store that data into each specific transient. Afterwards this data can be used
+to classify the data based on this information.
+
+"""
+
+
+class transientOberservation(object):
+
+
+    def __init__(time, light_value, color) : # init the time light value and color
+    """
+
+    this method is to be called on by the event broker when it wants
+    a transients time, light-value and color, it will return a tuple. 
+
+    """
